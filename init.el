f00d100d852901ea1a4843cb1eb9daa063201afc
Dan Korytov <dkorytov@uchicago.edu>
2019-01-13 21:15:16 -0600

    added unfill paragraph

--- init.el
+++ init.el
@@ -149,3 +149,13 @@ map))
                              "~/org/home.org"))
 (setq bell-volume 0)
 (setq visible-bell 1)
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph    
+    (defun unfill-paragraph (&optional region)
+      "Takes a multi-line paragraph and makes it into a single line of text."
+      (interactive (progn (barf-if-buffer-read-only) '(t)))
+      (let ((fill-column (point-max))
+            ;; This would override `fill-column' if it's an integer.
+            (emacs-lisp-docstring-fill-column t))
+        (fill-paragraph nil region)))
+    ;; Handy key definition
+    (define-key global-map "\M-Q" 'unfill-paragraph)
