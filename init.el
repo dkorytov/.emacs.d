6852eba33ceb406dbde81f0698dd3e058c87f4a3
Dan Korytov <dkorytov@uchicago.edu>
2019-01-21 16:15:55 -0600

    added package

--- init.el
+++ init.el
@@ -4,6 +4,13 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "http://melpa.milkbox.net/packages/")
+   t))
 
  (setq column-number-mode t)
 (defun prev-window ()
@@ -46,10 +53,13 @@ map))
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de" "a622aaf6377fe1cd14e4298497b7b2cae2efc9e0ce362dade3a58c16c89e089c" "2a9039b093df61e4517302f40ebaf2d3e95215cb2f9684c8c1a446659ee226b9" default)))
  '(org-agenda-files
    (quote
     ("~/org/core.org" "~/org/work.org" "~/org/school.org" "~/org/home.org")))
- '(package-selected-packages (quote (flylisp))))
+ '(package-selected-packages (quote (gruvbox-theme flylisp))))
 ;(custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
